df29ff4ed42975187fb9c4dcd91d343d817c2179
Chinseok Lee <allieuslee@gmail.com>
2015-04-16 18:46:51 +0900

    improved searching accuracy

--- Python2UnicodeFixer.py
+++ Python2UnicodeFixer.py
@@ -6,14 +6,16 @@ import re
 
 class Python2UnicodeFixer(sublime_plugin.EventListener):
     def on_post_save(self, view):
+        default_encoding = 'utf-8'
+
         if re.search(r'\.(py)$', view.file_name()):
             content = self.get_buffer_contents(view)
 
-            if not re.search('#\s*coding', content):
+            if not re.search(r'#\s*(-\*-)?\s*coding', content):
                 view.run_command("goto_line", {"line": 1})
-                view.run_command('insert_snippet', {"contents": "# -*- coding: utf-8 -*-\n"})
+                view.run_command('insert_snippet', {"contents": "# -*- coding: %s -*-\n" % default_encoding})
 
-            if 'unicode_literals' not in content:
+            if not re.search(r'from\s+__future__\s+import\s+unicode_literals', content):
                 view.run_command("goto_line", {"line": 2})
                 view.run_command('insert_snippet', {"contents": "from __future__ import unicode_literals\n"})
 
